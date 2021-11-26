c4e9ea0d6a9b150c23991631b02789e922531b20
OneTuskedMario <skygamegen@gmail.com>
2021-11-26 02:18:11 +0200

    Button, Wheel, Onclick

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var content = document.getElementById("wheel");
+var spin = document.getElementById("button");
+var delayInMilliseconds = 1000;
+
+
+spin.onclick = function() {
+  if (content.className == "") {
+    content.className = "moved";
+    setTimeout(function() {
+    content.className = "";
+}, delayInMilliseconds);
+  }
+};
