358754234aa4e42d79e0a01781411ac76e02fb40
OneTuskedMario <skygamegen@gmail.com>
2021-11-27 18:34:04 +0200

    random spin calc and hand

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
 var content = document.getElementById("wheel");
 var spin = document.getElementById("button");
-//var delayInMilliseconds = 1000;
+var tick = 0;
+var cycle = 4*360;
+var totalizer = 0;
 
+spin.onclick = function() { //button click function for spin
 
-spin.onclick = function() {
-  if (content.className == "") {
-    content.className = "moved";
-//    setTimeout(function() {
-//    content.className = "";
-//}, delayInMilliseconds);
+  // injects button styles in css to appear pressed in
+  spin.style.backgroundColor = "#476eb4";
+  spin.style.opacity = "0.6";
+  spin.style.boxShadow = "0 3px 1.5px #666";
+  spin.style.transform = "translate(0%, 14%)";
+  spin.style.pointerEvents = "none";
 
-} else {
-  content.className = "";
-}
+  //random int injected into the rotation for random spins
+  tick++;
+  cycles = Math.ceil(Math.random() * 360) + (cycle*tick); //random spins plus normal spins
+  content.style.transform = "rotate(" + cycles + "deg)"; //rotate the wheel
+  console.log(cycles);
+  var prize = Math.ceil((cycles % 360) / 45); //divide the wheel to determine prize
+  console.log(prize);
+};
+content.ontransitionend = function(){
+
+  //injects button styles in css to make button appear available again
+  spin.style.boxShadow = "0 5px 1.5px #999";
+  spin.style.opacity = "1";
+  spin.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  spin.style.transform = "translate(0%, 0%)";
+  spin.style.pointerEvents = "auto";
+
 };
